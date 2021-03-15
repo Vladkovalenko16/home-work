3dc1abe55351666c9f3c36e12d6d2daecc52c517
vladkovalenko16 <vladkovalenko16@mail.ru>
2021-03-15 19:02:11 +0300

    Добавил тестовый класс

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,6 +1,10 @@
 public class TestMain {
     //ctrl + K - коммит
     public static void main(String[] args) {
+        test1();
+    }
+
+    public static void test1() {
         System.out.println("Hello World");
     }
 }
