aa7ceaeb637ffc5793b60cbfcaaa38ceddff0280
vladkovalenko16 <vladkovalenko16@mail.ru>
2021-03-21 18:58:35 +0300

    исправил

--- src/Game.java
+++ src/Game.java
@@ -3,68 +3,70 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Game {
-    public static int MININT = 5;
-    public static int MAXINT = 6;
-    public static int POINT = 2;
+    public static final int MIN_NUMBER = 5;
+    public static final int MAX_NUMBER = 6;
+    public static final int MAX_POINT = 2;
 
-    public static void main (String [] args) throws IOException {
+    public static void main(String [] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Будем угадывать числа сначала я угадываю потом ты.");
-        System.out.println("Играем до " + POINT + " очков.");
-        System.out.println(("Загадывать числа от " + MININT + " до " + MAXINT));
-        int pointPerson = 0;
-        int pointComputer = 0;
-        boolean move = true;
-        do {
+        System.out.println("Играем до " + MAX_POINT + " очков.");
+        System.out.println(("Загадывать числа от " + MIN_NUMBER + " до " + MAX_NUMBER));
+        int personsPoint = 0;
+        int computersPoint = 0;
+        //do {
+        for (int turn = 1; personsPoint != MAX_POINT && computersPoint != MAX_POINT; turn++) {
             int numberPerson;
             int numberComputer;
-            if (move){
+            if (turn % 2 != 0) {
                 numberPerson = readInt(in, "Введи свое число");
-                numberComputer = findRandomNumber("Вот мое число: ");
+                numberComputer = generateRandomNumber();
+                System.out.println("Вот мое число " +numberComputer);
                 if (numberComputer == numberPerson) {
-                    pointComputer++;
+                    computersPoint++;
                     System.out.println(" Я угадал.");
                 }
-                if (pointComputer == POINT) move = false;
-                System.out.println("Счет " + pointComputer + ":" + pointPerson);
             }
-            if (move){
+            if (turn % 2 == 0) {
                 System.out.println("Я загодал число.");
                 numberPerson = readInt(in, "Введи свое число");
-                numberComputer = findRandomNumber("Вот мое число: ");
+                numberComputer = generateRandomNumber();
+                System.out.println("Вот мое число " + numberComputer);
                 if (numberComputer == numberPerson) {
-                    pointPerson++;
+                    personsPoint++;
                     System.out.println(" Ты угадал.");
                 }
-                if (pointPerson == POINT) move = false;
             }
-            System.out.println("Счет " + pointComputer + ":" + pointPerson);
-        } while (move);
-        if (pointComputer == POINT) System.out.println("Я победил)) LOL.");
-        if (pointPerson == POINT) System.out.println(("Поздравляю."));
-
-    }
-    public static int findRandomNumber (String string){
-        int randomNumber = (int) Math.round((Math.random() * (MAXINT - MININT) + MININT));
-        System.out.println(string + randomNumber);
-        return randomNumber;
+            System.out.println("Счет " + computersPoint + ":" + personsPoint);
+        } //while (personsPoint != MAX_POINT && computersPoint != MAX_POINT);
+        if (computersPoint == MAX_POINT) {
+            System.out.println("Я победил)) LOL.");
+        }else {
+            System.out.println(("Поздравляю."));
+        }
     }
 
-    public static int readInt (BufferedReader in, String message) throws IOException {
+    public static int generateRandomNumber(){
+        return (int) Math.round((Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER));
+    }
+
+    public static int readInt(BufferedReader in, String message) throws IOException {
         int number = 0;
         System.out.println(message);
-        boolean hasNumber = true;
+        boolean hasNumber = false;
         do {
             try {
                 String string = in.readLine();
                 number = Integer.parseInt(string);
-                if (MININT <= number && number <= MAXINT){
-                    hasNumber = false;
-                } else System.out.println("Введи число от " + MININT + " до " + MAXINT);
+                if (MIN_NUMBER <= number && number <= MAX_NUMBER) {
+                    hasNumber = true;
+                } else {
+                    System.out.println("Введи число от " + MIN_NUMBER + " до " + MAX_NUMBER);
+                }
             } catch (NumberFormatException e){
                 System.out.println("Нужно целое число");
             }
-        }while (hasNumber);
+        }while (!hasNumber);
         return number;
     }
 }
