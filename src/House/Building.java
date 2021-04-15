976da2b79e5cbbd022a5a3ec5c47274f754af524
vladkovalenko16 <vladkovalenko16@mail.ru>
2021-04-16 00:57:17 +0300

    добавил метод заселить

--- src/House/Building.java
+++ src/House/Building.java
@@ -4,57 +4,79 @@ import java.util.Arrays;
 
 public class Building {
     public final double PRISE_PER_METER = 2;
-    public Apartment[] apartments;
+    public  Apartment[] apartments;
 
-    public Building() {
+    public Building (){
     }
 
-    public Building(Apartment[] apartments) {
+    public Building (Apartment[] apartments){
         this.apartments = apartments;
     }
 
-    public void makeListAllResidents() {
+    public void  makeListAllResidents(){
         for (int i = 0; i < apartments.length; i++) {
             System.out.println(apartments[i].resident.name);
         }
     }
 
-    public String lookingResidentInApartment(int numberOfApartment) {
+    public  String lookingResidentInApartment(int numberOfApartment){
         String residentName = null;
         for (int i = 0; i < apartments.length; i++) {
-            if (apartments[i].apartmentsNumber == numberOfApartment) {
+            if(apartments[i].apartmentsNumber == numberOfApartment){
                 residentName = apartments[i].resident.name;
                 break;
             }
         }
         return residentName;
     }
-
-    public Apartment[] sortsApartmentsBySquare(Apartment[] apartment) {
-        for (int i = apartment.length - 1; i > 0; i--) {
+    public Apartment[] sortsApartmentsBySquare(Apartment[] apartments){
+        if(apartments.length < 2){
+            return  apartments;
+        }
+        for (int i = apartments.length - 1; i > 0 ; i--) {
             for (int j = 0; j < i; j++) {
-                if (apartment[j].apartmentsSquare() > apartment[j + 1].apartmentsSquare()) {
-                    Apartment apartment1 = apartment[j];
-                    apartment[j] = apartment[j + 1];
-                    apartment[j + 1] = apartment1;
+                double a = apartments[j].apartmentsSquare();
+                double b = apartments[j + 1].apartmentsSquare();
+                if (apartments[j].apartmentsSquare() < apartments[j +1].apartmentsSquare()){
+                    Apartment apartment1 = apartments[j];
+                    apartments[j] = apartments[j +1];
+                    apartments[j + 1] = apartment1;
                 }
             }
         }
-        return apartment;
+        return apartments;
     }
-
-    public Resident[] sortResidentsByMoney(Resident[] residents) {
-        for (int i = residents.length - 1; i > 0; i--) {
+    public Resident[] sortResidentsByMoney(Resident[] residents){
+        if (residents.length < 2) {
+            return residents;
+        }
+        for (int i = residents.length-1; i > 0 ; i--) {
             for (int j = 0; j < i; j++) {
-                if (residents[j].money > residents[j + 1].money) {
-                    double money = residents[j].money;
-                    residents[j].money = residents[j + 1].money;
-                    residents[j + 1].money = money;
+                if (residents[j].money < residents[j + 1].money){
+                    Resident resident = residents[j];
+                    residents[j] = residents[j + 1];
+                    residents[j + 1] = resident;
                 }
             }
         }
         return residents;
     }
+    public Resident[] moveInResidents(Resident[] residents){
+        apartments = sortsApartmentsBySquare(apartments);
+        residents = sortResidentsByMoney(residents);
+        for (int i = 0; i < apartments.length ; i++) {
+            if (apartments[i].checkingResident){
+                    if (residents.length == 0) break;
+                    apartments[i].moveInResident(residents[0]);
+                    residents = Arrays.copyOfRange(residents, 1, residents.length);
+            }
+        }
+        return residents;
+    }
+
+
+
+
 
 
 }
